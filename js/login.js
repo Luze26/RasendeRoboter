66da5a38b8a867f1515a8030b79eb541184368e4
kraiss <rassat.pierrick@gmail.com>
2013-11-15 22:19:07 +0100

    Cliquer sur les icones de parties permet de selectionner une partie

--- js/login.js
+++ js/login.js
@@ -9,12 +9,21 @@ function init() {
 									button.setAttribute('type', 'button');
 									button.classList.add('btn');
 									button.classList.add('btn-default');
+                                    button.onclick = function() { 
+                                        var game = data.gamesList[p];
+                                        return function() {selectGame(game);}
+                                    }();
 									div.appendChild(button);
 									button.appendChild( document.createTextNode(data.gamesList[p]));
 								}
 							}
 			 );
 	socket.emit('loginPage');
+}
+
+
+function selectGame(game) {
+    document.getElementById('idGame').value = game;
 }
 
 
