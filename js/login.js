e1094c8a5c4d70bd6e699e9f09c29ab770fc14c7
kraiss <rassat.pierrick@gmail.com>
2013-11-16 01:11:11 +0100

    Refactoring du js login.js harmonisation des templates des pages web (headers, positionnement, ...)

--- js/login.js
+++ js/login.js
@@ -2,28 +2,27 @@ function init() {
 	// Connect to the SocketIO server to retrieve ongoing games.
 	socket = io.connect();
 	socket.on('gamesList', function(data) {
-								var div = document.getElementById('lesParties');
-								div.innerHTML='';
-								for(p in data.gamesList) {
-									var button = document.createElement('button');
-									button.setAttribute('type', 'button');
-									button.classList.add('btn');
-									button.classList.add('btn-default');
-                                    button.onclick = function() { 
-                                        var game = data.gamesList[p];
-                                        return function() {selectGame(game);}
-                                    }();
-									div.appendChild(button);
-									button.appendChild( document.createTextNode(data.gamesList[p]));
-								}
-							}
-			 );
+        
+        var div = document.getElementById('lesParties');
+        div.innerHTML='';
+        
+        for(g in data.gamesList) {
+            var button = document.createElement('button');
+            button.setAttribute('type', 'button');
+            button.classList.add('btn');
+            button.classList.add('btn-default');
+            
+            // Fill the input text when you click on a game
+            button.onclick = function() { 
+                var game = data.gamesList[g];
+                return function() {
+                    document.getElementById('idGame').value = game;
+                }
+            }();
+            
+            div.appendChild(button);
+            button.appendChild( document.createTextNode(data.gamesList[g]));
+        }
+    });
 	socket.emit('loginPage');
 }
-
-
-function selectGame(game) {
-    document.getElementById('idGame').value = game;
-}
-
-
